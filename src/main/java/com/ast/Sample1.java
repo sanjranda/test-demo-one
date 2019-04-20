98b2a2b74d97a1cb051b9377c429a32f269fa80e
SERENECORP\sksharma <sksharma@astcorporation.com>
2019-04-20 13:02:05 +0530

    added new class and method

--- src/main/java/com/ast/Sample1.java
+++ src/main/java/com/ast/Sample1.java
@@ -4,5 +4,12 @@ public class Sample1 {
 
 	public static void main(String[] sanj) {
 		System.out.println("this is a test Sample1 class .... ");
+		ABC.display1();
+	}
+	
+	public static class ABC {
+	public static void display1() {
+		System.out.println("this is static class a ....");
+		}
 	}
 }
