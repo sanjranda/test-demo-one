18a43cd0084067138df616d5d420148b05e49cab
SERENECORP\sksharma <sksharma@astcorporation.com>
2019-04-20 21:18:37 +0530

    updated java class commented

--- src/main/java/com/ast/Sample1.java
+++ src/main/java/com/ast/Sample1.java
@@ -4,12 +4,12 @@ public class Sample1 {
 
 	public static void main(String[] sanj) {
 		System.out.println("this is a test Sample1 class .... ");
-		ABC.display1();
+		//ABC.display1();
 	}
 	
-	public static class ABC {
+	/*public static class ABC {
 	public static void display1() {
 		System.out.println("this is static class a ....");
 		}
-	}
+	}*/
 }
