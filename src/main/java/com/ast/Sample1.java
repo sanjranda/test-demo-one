e965d1adb26c56a91ad28cf9fb237d4c972e9f5e
SERENECORP\sksharma <sksharma@astcorporation.com>
2019-04-22 17:08:11 +0530

    removed commented code from java file

--- src/main/java/com/ast/Sample1.java
+++ src/main/java/com/ast/Sample1.java
@@ -4,12 +4,6 @@ public class Sample1 {
 
 	public static void main(String[] sanj) {
 		System.out.println("this is a test Sample1 class .... ");
-		//ABC.display1();
 	}
 	
-	/*public static class ABC {
-	public static void display1() {
-		System.out.println("this is static class a ....");
-		}
-	}*/
 }
