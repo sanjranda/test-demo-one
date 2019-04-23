91aadafb72040c31ac67b712986368d6c8d2b1dd
SERENECORP\sksharma <sksharma@astcorporation.com>
2019-04-23 20:22:09 +0530

    included multiple sysout statements.

--- src/main/java/com/ast/Sample1.java
+++ src/main/java/com/ast/Sample1.java
@@ -4,6 +4,12 @@ public class Sample1 {
 
 	public static void main(String[] sanj) {
 		System.out.println("this is a test Sample1 class .... ");
+		test();
+	}
+	
+	public static void test() {
+		System.out.println("inside test");
+		System.out.println("inside test one");
 	}
 	
 }
