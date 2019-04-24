8dbc820d7fec704c28eb7d047e85eb27dd9d7fe3
SERENECORP\sksharma <sksharma@astcorporation.com>
2019-04-24 20:34:07 +0530

    changes made

--- src/main/java/com/ast/Sample1.java
+++ src/main/java/com/ast/Sample1.java
@@ -4,6 +4,12 @@ public class Sample1 {
 
 	public static void main(String[] sanj) {
 		System.out.println("this is a test Sample1 class .... ");
-		
+		test();
+	}
+	
+	public static void test() {
+		System.out.println("this is test one ...");
+		System.out.println("this is test two ...");
+		System.out.println("this is test three ....");
 	}
 }
