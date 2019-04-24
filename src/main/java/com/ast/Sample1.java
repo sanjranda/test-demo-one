0205e9e45941f35e2d85ef495a017f2824c5e499
SERENECORP\sksharma <sksharma@astcorporation.com>
2019-04-24 19:05:04 +0530

    removed multiple sysout statements

--- src/main/java/com/ast/Sample1.java
+++ src/main/java/com/ast/Sample1.java
@@ -4,12 +4,6 @@ public class Sample1 {
 
 	public static void main(String[] sanj) {
 		System.out.println("this is a test Sample1 class .... ");
-		test();
 	}
-	
-	public static void test() {
-		System.out.println("inside test");
-		System.out.println("inside test one");
-	}
-	
+		
 }
