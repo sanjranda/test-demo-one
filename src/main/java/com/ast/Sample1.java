fc2b8184fb39c96db324f94199a8a4a2f564d048
SERENECORP\sksharma <sksharma@astcorporation.com>
2019-04-29 22:34:47 +0530

    removed sysout from sample1.java

--- src/main/java/com/ast/Sample1.java
+++ src/main/java/com/ast/Sample1.java
@@ -3,13 +3,7 @@ package com.ast;
 public class Sample1 {
 
 	public static void main(String[] sanj) {
-		System.out.println("this is a test Sample1 class .... ");
-		test();
+		System.out.println("this is a test Sample1 class .... ");		
 	}
 	
-	public static void test() {
-		System.out.println("this is test one ...");
-		System.out.println("this is test two ...");
-		System.out.println("this is test three ....");
-	}
 }
